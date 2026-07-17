38320fd85c3c4548a159d8167e19619e031f4041
Tshering00 <tshering.work1@gmail.com>
2026-07-17 08:08:19 +0545

    feat: Add cancel and resend feature

--- backend/apps/emails/views.py
+++ backend/apps/emails/views.py
@@ -1,8 +1,13 @@
 # emails/views.py
+from django.utils import timezone
 from .tasks import send_email_task
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 from .models import EmailSchedule, Status
 from .serializers import EmailScheduleSerializer
+from celery import current_app
 
 class EmailScheduleViewSet(viewsets.ModelViewSet):
     serializer_class = EmailScheduleSerializer
@@ -24,3 +29,71 @@ class EmailScheduleViewSet(viewsets.ModelViewSet):
         email.celery_task_id = task.id
         email.status = Status.QUEUED
         email.save(update_fields=['celery_task_id', 'status'])
+
+    def perform_destroy(self, instance):
+        """
+        Cleans up background resources when a schedule is deleted.
+        If the email is still pending execution, revoke it from Celery.
+        """
+         # If the email is active (PENDING/QUEUED) and has a task ID, revoke it from Celery queue
+        if instance.status in [Status.PENDING, Status.QUEUED] and instance.celery.task_id:
+            current_app.cotrol.revoke(instance.celery_task_id, terminate=True)
+
+             # Delete the email record from the database (always executed)
+            instance.delete()
+
+    @action(detail=True, methods=['POST'])
+    def cancel(self, request, pk=None):
+        email = self.get_object()
+
+           # Only allow cancellation if the email is not already processing, sent, or failed
+        if email.status not in [Status.PENDING, Status.QUEUED]:
+            return Response(
+                {"error": f"Cannot cancel an email that is already {email.status.lower()}. "},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+           # Revoke the background task from the Celery broker if it exists
+        if email.celery_task_id:
+            current_app.control.revoke(email.celery_task_id, terminate=True)
+
+             # Always update status to CANCELLED in DB
+            email.status = Status.CANCELLED
+            email.save(update_fields=['status'])
+
+            return Response(
+                {"message": "Email schedule has been cancelled successfully."},
+                status=status.HTTP_200_OK
+            )
+
+    @action(detail=True, methods=['POST'])
+    def resend(self, request, pk=None):
+        # custom endpoints to retry or resend an email
+        email = self.get_object()
+
+        if email.status in [Status.PENDING, Status.QUEUED, Status.PROCESSING]:
+            return Response(
+                {"error": f"Cannot resend an email that is currently in {email.status.lower()}."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        new_scheduled_at = request.data.get('scheduled_at')
+        if new_scheduled_at:
+            email.scheduled_at = new_scheduled_at
+            task = send_email_task.apply_async(
+                args=[str(email.id)],
+                eta=email.scheduled_at
+            )
+        else:
+            email.scheduled_at = timezone.now()
+            task = send_email_task.delay(str(email.id))
+
+         #reset status for retry
+        email.status = Status.QUEUED
+        email.celery_task_id = task.id
+        email.sent_at = None
+        email.error_message = None
+        email.save(update_fields=['status', 'celery_task_id', 'scheduled_at', 'sent_at', 'error_message'])
+
+        #return updated email schedule object
+        serializer = self.get_serializer(email)
+        return Response(serializer.data, status=status.HTTP_200_OK)   
